aff2b77657cb89d19e57f3dd3e34e394fffb4bdc
05-ram <ravi.karthik@aximsoft.com>
2025-02-04 13:40:27 +0530

    framer motion added

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,15 +1,24 @@
-import storeImg from "../assets/images/store-img.jpg"
+import storeImg from "../assets/images/store-img.jpg";
+import { motion } from 'framer-motion';
 const Home = () => {
     return (
         <div className="d-flex justify-content-between calc-height align-items-center home-page">
-            <div className="home-left">
+            <motion.div
+                initial={{ opacity: 0.5, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+                className="home-left">
                 <div className="home-title">Book Shop</div>
                 <div className="desc mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur veritatis odit porro illo laboriosam, accusantium doloribus sapiente possimus excepturi quibusdam harum vero fugit suscipit sed mollitia iste, at magni est?</div>
-            </div>
-            <div className="home-right">
+            </motion.div>
+            <motion.div
+                initial={{ opacity: 0.5, scale: 0 }}
+                whileInView={{ opacity: 1, scale: 1 }}
+                transition={{ duration: 1.4, ease: "easeInOut" }}
+                className="home-right">
                 <img src={storeImg} alt="store-img" />
-            </div>
-        </div>
+            </motion.div>
+        </div >
     )
 }
 
